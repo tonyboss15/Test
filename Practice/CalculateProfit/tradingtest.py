8ef685ac0b70485e4dfbe39e8ee5d96ffca4a98e
Tonyboss15-PC <tonyboss15@gmail.com>
2020-05-18 07:31:41 -0400

    s

--- Practice/CalculateProfit/tradingtest.py
+++ Practice/CalculateProfit/tradingtest.py
@@ -5,5 +5,8 @@ import time
 api_key = 'RNZPXZ6Q9FEFMEHM'
 
 ts = TimeSeries(key=api_key, output_format='pandas')
-data, meta_data = ts.get_intraday(symbol='MSFT', interval = '1min', outputsize = 'full')
+data, meta_data = ts.get_intraday(symbol='MSFT', interval = '60min', outputsize = 'full')
 print(data)
+close_data = data['4. close']
+percentage_change = close_data.pct_change()
+print(percentage_change)
